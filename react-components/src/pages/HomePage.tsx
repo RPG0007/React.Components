8733603437bfd141ac8b077e3c10d69cacbcb403
RPG0007 <90514460+RPG0007@users.noreply.github.com>
2023-03-19 14:20:38 +0300

    feat:add card to homepage

--- react-components/src/pages/HomePage.tsx
+++ react-components/src/pages/HomePage.tsx
@@ -1,11 +1,27 @@
+import { CardList } from 'components/Card';
 import React, { Component } from 'react';
+// import { Link } from 'react-router-dom';
+import glass from '../assets/search_glass.png';
 
 export class HomePage extends Component {
   render() {
     return (
-      <div>
-        <h1>This is the HOME page</h1>
+      <div className="container_home">
+        <h2>Store</h2>
+        <SearchBar />
+        <CardList />
       </div>
+    );
+  }
+}
+
+class SearchBar extends Component {
+  render() {
+    return (
+      <form className="searchBar">
+        <input type="text" placeholder="Search..." />
+        <img className="searchGlass" src={glass} alt="search image" />
+      </form>
     );
   }
 }
