0259194325c72a5979b89cd4c357a53cca90a3d5
RPG0007 <90514460+RPG0007@users.noreply.github.com>
2023-03-19 15:11:24 +0300

    feat:add components and css change

--- react-components/src/components/Footer.tsx
+++ react-components/src/components/Footer.tsx
@@ -6,14 +6,14 @@ export class Footer extends Component {
   render() {
     return (
       <footer className="footer">
-        <a href="https://github.com/Siaw4ik">
+        <a href="https://github.com/rpg0007">
           <img className="githubIcon" src={githubIcon} alt="github link" />
         </a>
         <div className="footer_description">
           <p>Online Store 2023</p>
           <p>React</p>
         </div>
-        <a href="https://rs.school/js/">
+        <a href="https://rs.school/react/">
           <img className="rssIcon" src={rssIcon} alt="RSSchool link" />
         </a>
       </footer>
--- react-components/src/pages/HomePage.tsx
+++ react-components/src/pages/HomePage.tsx
@@ -1,27 +1,15 @@
-import { CardList } from 'components/Card';
+import { CardList } from '../components/CardList';
 import React, { Component } from 'react';
-// import { Link } from 'react-router-dom';
-import glass from '../assets/search_glass.png';
+import { SearchBar } from '../components/SearchBar';
 
 export class HomePage extends Component {
   render() {
     return (
       <div className="container_home">
-        <h2>Store</h2>
+        <h2 data-testid="homepage-h1">Store</h2>
         <SearchBar />
         <CardList />
       </div>
-    );
-  }
-}
-
-class SearchBar extends Component {
-  render() {
-    return (
-      <form className="searchBar">
-        <input type="text" placeholder="Search..." />
-        <img className="searchGlass" src={glass} alt="search image" />
-      </form>
     );
   }
 }
